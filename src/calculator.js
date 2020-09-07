17b94940d5756b2f4a3c5e0204cf76e8cc162046
Mare <sevilamare@gmail.com>
2020-09-07 17:09:18 -0300

    calculator

---
+++ src/calculator.js
@@ -0,0 +1,14 @@
+const calculator = (() => {
+  const sum = (...args) => args.reduce((a, b) => (a+b))
+
+  const sub = (...args) => args.reduce((a, b) => (a-b))
+
+  const div = (...args) => args.reduce((a, b) => (a/b))
+
+  const mult = (...args) => args.reduce((a, b) => (a*b))
+
+  return {sum, sub, div, mult}
+} )();
+
+// export {sum, sub, dib, mult}
+export default calculator;
---
+++ src/calculator.test.js
@@ -0,0 +1,19 @@
+import calculator from './calculator.js'
+
+describe('test suit for calculator', () => {
+  it ('should sum all numbers', () => {
+    expect(calculator.sum(1,2,3)).toBe(6)
+  })
+  
+  it ('should sum all numbers', () => {
+    expect(calculator.sub(12,6)).toBe(6)
+  })
+  
+  it ('should sum all numbers', () => {
+    expect(calculator.div(12,2)).toBe(6)
+  })
+  
+  it ('should sum all numbers', () => {
+    expect(calculator.mult(3,2)).toBe(6)
+  })
+})
