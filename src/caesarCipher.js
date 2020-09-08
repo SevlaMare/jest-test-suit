71e35f8177807cabb64cf32a5ad1e0c360579ffa
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-09-08 09:48:50 -0500

    Refactor caesar cipher.

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,28 +1,37 @@
 const CesarCipher = (() => {
+  const MAX_NUMBER_LETTER = 122;
+  const MIN_NUMBER_LETTER = 96;
+
   const toLetter = (num) => String.fromCharCode(num);
 
   const toNum = (str, pos) => str.charCodeAt(pos);
 
   const decryptLetterNumber = (letterNumber, timesToUnshift) => {
     let decryptedLetter = letterNumber - timesToUnshift;
-    while (decryptedLetter < 97) {
-      decryptedLetter = 122 - (96 - decryptedLetter);
+    while (decryptedLetter <= MIN_NUMBER_LETTER) {
+      decryptedLetter = MAX_NUMBER_LETTER - (MIN_NUMBER_LETTER - decryptedLetter);
     }
     return decryptedLetter;
   };
 
   const encryptLetterNumber = (letterNumber, timesToShift) => {
     let encryptedLetter = letterNumber + timesToShift;
-    while (encryptedLetter > 122) {
-      encryptedLetter = 96 + (encryptedLetter - 122);
+    while (encryptedLetter > MAX_NUMBER_LETTER) {
+      encryptedLetter = MIN_NUMBER_LETTER + (encryptedLetter - MAX_NUMBER_LETTER);
     }
     return encryptedLetter;
   };
 
+  const encryptLetter = (string, index, numToShift) => 
+    toLetter(encryptLetterNumber(toNum(string, index), numToShift));
+
+  const decryptLetter = (string, index, numToUnShift) => 
+    toLetter(decryptLetterNumber(toNum(string, index), numToUnShift));
+
   const encrypt = (string, numToShift) => {
     let encryptWord = '';
     for (let index = 0; index < string.length; index += 1) {
-      encryptWord += toLetter(encryptLetterNumber(toNum(string, index), numToShift));
+      encryptWord += encryptLetter(string, index, numToShift);
     }
     return encryptWord;
   };
@@ -30,7 +39,7 @@ const CesarCipher = (() => {
   const decrypt = (string, numToUnShift) => {
     let decryptWord = '';
     for (let index = 0; index < string.length; index += 1) {
-      decryptWord += toLetter(decryptLetterNumber(toNum(string, index), numToUnShift));
+      decryptWord += decryptLetter(string, index, numToUnShift);
     }
     return decryptWord;
   };
