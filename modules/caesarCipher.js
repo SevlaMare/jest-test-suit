9da2690d65fb6dcda9c157ddc92a843c06723202
Oscar Alfredo Gómez Elizarrarás <elizarrarasalfredoog@gmail.com>
2020-09-08 15:48:09 -0500

    Add cases for punctuation.

--- modules/caesarCipher.js
+++ modules/caesarCipher.js
@@ -1,46 +1,65 @@
 const CesarCipher = (() => {
   const MAX_NUMBER_LETTER = 122;
   const MIN_NUMBER_LETTER = 96;
+  const MAX_NUMBER_PUNCTUATION = 64;
+  const MIN_NUMBER_PUNCTUATION = 32;
 
   const toLetter = (num) => String.fromCharCode(num);
 
   const toNum = (str, pos) => str.charCodeAt(pos);
 
-  const decryptLetterNumber = (letterNumber, timesToUnshift) => {
-    let decryptedLetter = letterNumber - timesToUnshift;
-    while (decryptedLetter <= MIN_NUMBER_LETTER) {
-      decryptedLetter = MAX_NUMBER_LETTER - (MIN_NUMBER_LETTER - decryptedLetter);
-    }
-    return decryptedLetter;
-  };
-
   const encryptLetterNumber = (letterNumber, timesToShift) => {
     let encryptedLetter = letterNumber + timesToShift;
-    while (encryptedLetter > MAX_NUMBER_LETTER) {
-      encryptedLetter = MIN_NUMBER_LETTER + (encryptedLetter - MAX_NUMBER_LETTER);
+    while (
+      encryptedLetter > MAX_NUMBER_LETTER ||
+      encryptedLetter <= MIN_NUMBER_LETTER
+    ) {
+      if (encryptedLetter > MAX_NUMBER_LETTER) {
+        encryptedLetter =
+          MIN_NUMBER_LETTER + (encryptedLetter - MAX_NUMBER_LETTER);
+      } else {
+        encryptedLetter =
+          MAX_NUMBER_LETTER - (MIN_NUMBER_LETTER - encryptedLetter);
+      }
     }
     return encryptedLetter;
   };
 
-  const encrypt = (string, numToShift) => {
-    let encryptWord = '';
+  const encryptPunctuationToNumber = (punctuationNumber, timesToShift) => {
+    let encryptedPunctuation = punctuationNumber + timesToShift;
+    while (
+      encryptedPunctuation > MAX_NUMBER_PUNCTUATION ||
+      encryptedPunctuation <= MIN_NUMBER_PUNCTUATION
+    ) {
+      if (encryptedPunctuation > MAX_NUMBER_PUNCTUATION) {
+        encryptedPunctuation =
+          MIN_NUMBER_PUNCTUATION + (encryptedPunctuation - MAX_NUMBER_PUNCTUATION);
+      } else {
+        encryptedPunctuation =
+          MAX_NUMBER_PUNCTUATION - (MIN_NUMBER_PUNCTUATION - encryptedPunctuation);
+      }
+    }
+    return encryptedPunctuation;
+  };
+
+  const cipher = (string, numToShift) => {
+    let encryptWord = "";
     for (let index = 0; index < string.length; index += 1) {
-      encryptWord += toLetter(encryptLetterNumber(toNum(string, index), numToShift));
+      if (string[index].match(/[a-z]/)) {
+        encryptWord += toLetter(
+          encryptLetterNumber(toNum(string, index), numToShift)
+          );
+      } else {
+        encryptWord += toLetter(
+          encryptPunctuationToNumber(toNum(string, index), numToShift)
+          );
+      }
     }
     return encryptWord;
   };
 
-  const decrypt = (string, numToUnShift) => {
-    let decryptWord = '';
-    for (let index = 0; index < string.length; index += 1) {
-      decryptWord += toLetter(decryptLetterNumber(toNum(string, index), numToUnShift));
-    }
-    return decryptWord;
-  };
-
   return {
-    encrypt,
-    decrypt,
+    cipher,
   };
 })();
 
--- test/caesarCipher.test.js
+++ test/caesarCipher.test.js
@@ -2,18 +2,18 @@ import CesarCipher from '../modules/caesarCipher';
 
 describe('should encrypt and decrypt correctly', () => {
   it('should encrypt abz to bca', () => {
-    expect(CesarCipher.encrypt('abz', 1)).toBe('bca');
+    expect(CesarCipher.cipher('abz', 1)).toBe('bca');
   });
 
   it('should decrypt bca to abz', () => {
-    expect(CesarCipher.decrypt('bca', 1)).toBe('abz');
+    expect(CesarCipher.cipher('bca', -1)).toBe('abz');
   });
 
   it('should encrypt correctly with punctuation', () => {
-    expect(CesarCipher.encrypt('!.,?', 1)).toBe('"/-@');
+    expect(CesarCipher.cipher('!.,?', 1)).toBe('"/-@');
   });
 
   it('should decrypt correctly with punctuation', () => {
-    expect(CesarCipher.decrypt('"/-@', 1)).toBe('!.,?');
+    expect(CesarCipher.cipher('"/-@', -1)).toBe('!.,?');
   });
 });
